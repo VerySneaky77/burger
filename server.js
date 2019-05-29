343dad9dc1fbb03bd943c3854aa530aa4aa90e54
Carlos Quinto <carlquin77@gmail.com>
2019-05-29 14:02:56 -0700

    Included PORT to server.js to host

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 const express = require("expresss");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.listen(PORT, function () {
+    console.log("App now listening at localhost:" + PORT);
+});
