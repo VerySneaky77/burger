71819d2b9e437c64f567703a5a552eaac4d9f777
Carlos Quinto <carlquin77@gmail.com>
2019-05-29 13:48:32 -0700

    Added config file structure

--- db/schema.sql
+++ db/schema.sql
@@ -2,9 +2,9 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-    id INT AUTO_INCREMENT,
-    burger_name,
-    devoured
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(100) NOT NULL,
+    devoured BOOLEAN,
 
     PRIMARY KEY(id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Royale with Cheese", false), ("Baconzilla", true), ("Plain", false);
